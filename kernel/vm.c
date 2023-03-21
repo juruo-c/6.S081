671eef88fc151565b0f81f299de19571907379f3
juruo-c <2915601267@qq.com>
2023-03-21 10:17:45 +0800

    23.03.21 Pass pgtbl:vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,31 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void 
+vmprint_helper(pagetable_t pgtbl, int level) 
+{
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pgtbl[i];
+    if(pte & PTE_V){
+      for (int j = 0; j < level; j ++ ) {
+        if (j) printf(" ");
+        printf("..");
+      }
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+      if ((pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+        // this PTE points to a lower-level page table.
+        uint64 child = PTE2PA(pte);
+        vmprint_helper((pagetable_t)child, level + 1);
+      }
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pgtbl) 
+{
+  printf("page table %p\n", pgtbl);
+  vmprint_helper(pgtbl, 1);
+}
